318c4fc8f8c557fe63e44dd12e2b488abda79c64
Bryan Sanchez <bmsa753@gmail.com>
2021-07-22 16:23:11 -0500

    nuevos tests

--- src/test/java/IntegracionJenkins/RelationalOperatorTest.java
+++ src/test/java/IntegracionJenkins/RelationalOperatorTest.java
@@ -17,16 +17,28 @@ public class RelationalOperatorTest {
 		RelationalOperator tester = new RelationalOperator();
 		assertTrue(tester.isGreater(2, 1));
 	}
+	
+	@Test
+	public void isGreaterTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertFalse(tester.isGreater(2, 2));
+	}
 
 	@Test
 	public void isLessTest1() {
 		RelationalOperator tester = new RelationalOperator();
-		assertTrue(tester.isLess(4, 4));
+		assertFalse(tester.isLess(4, 4));
 	}
 	
 	@Test
 	public void isLessTest2() {
 		RelationalOperator tester = new RelationalOperator();
 		assertFalse(tester.isLess(5, 1));
+	}
+	
+	@Test
+	public void isLessTest3() {
+		RelationalOperator tester = new RelationalOperator();
+		assertTrue(tester.isLess(1, 3));
 	}
 }
